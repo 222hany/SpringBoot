034d5841912f2fbab7d26d94eb3c0f145e0c73bb
222hany <143372305+222hany@users.noreply.github.com>
2023-12-06 17:45:04 +0900

    springboot

---
+++ JPA-ImgUploadList/src/main/java/com/kh/springdb/controller/ItemController.java
@@ -0,0 +1,59 @@
+package com.kh.springdb.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
+
+import com.kh.springdb.model.Item;
+import com.kh.springdb.service.ItemService;
+
+import lombok.*;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+	private final ItemService itemService;
+	
+	//메인페이지
+	@GetMapping("/")
+	public String mainPage(Model model) {
+		List<Item> items = itemService.allItemView();
+		model.addAttribute("items", items);
+		return "index";
+	}
+	
+	//상품 전체목록 페이지로 이동하기위한 GetMapping
+	@GetMapping("/item/list")
+	public String itemList(Model model) {
+		List<Item> items = itemService.allItemView();
+		model.addAttribute("items", items);
+		return "itemList";
+	}
+	
+	//상품 상세보기
+	@GetMapping("/item/details/{id}")
+	public String itemDetails(@PathVariable("id") int id, Model model) {
+		Item item = itemService.getItemById(id);
+		model.addAttribute("item", item);
+		return "itemDetails";
+	}
+	
+	//상품등록 페이지로 이동
+	@GetMapping("/item/new")
+	public String itemSaveForm(Model model) {
+		return "addItemForm";
+	}
+	
+	//클라이언트가 등록한 상품등록 내용을 DB 에 업로드
+	@PostMapping("/item/new")
+	public String itemSave(Item item, MultipartFile imgFile) throws IllegalStateException, IOException {
+		itemService.saveItem(item, imgFile);
+		return "redirect:/itemList";
+	}
+}
---
+++ JPA-ImgUploadList/src/main/java/com/kh/springdb/model/Item.java
@@ -0,0 +1,43 @@
+package com.kh.springdb.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Item {
+	//id name text price count stock isSoldOut
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="item_seq")
+	@SequenceGenerator(name="item_seq", sequenceName="item_seq", allocationSize=1)
+	private int id;
+	
+	private String name; //상품이름
+	private String text; //상품설명
+	private int price; //상품가격
+	private int count; //판매개수
+	private int stock; //재고
+	private int isSoldOut; //상품 품절 유무
+	
+	//이미지 업로드를 위한 파일명, 이미지 경로, 상품등록 날짜
+	private String imgName;
+	private String imgPath;
+	
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	private LocalDate createDate;
+	
+	@PrePersist //DB 에 값을 넣을 때 자동으로 생성된 날짜가 들어감
+	public void createDate() {
+		this.createDate = LocalDate.now();
+	}
+	
+	//판매자가 누구인지, 장바구니에 어떤 아이템이 들어가있는지 아직 작성하지 않음
+}
